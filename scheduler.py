8120467bcea9c41b4ffff97b019822bd0134329e
YoojLee <daebaq27@gmail.com>
2022-07-29 00:45:48 +0000

    Fix cosine decay and Add cosine annealing

--- scheduler.py
+++ scheduler.py
@@ -29,23 +29,56 @@ class WarmupLinearDecay(LambdaLR):
 class WarmupCosineDecay(LambdaLR):
     """
     Linear warmup and then cosine decay.
-
-    cosine decay?
-
     """
-    def __init__(self, optimizer, warmup_steps, total_steps, cycles=.5, last_epoch=-1, verbose=False):
+    def __init__(self, optimizer, warmup_steps, total_steps, cycle_factor=1., last_epoch=-1, verbose=False):
+        """
+        - Args
+            optimizer: optimizer to adjust learning rate
+            warmup_steps: steps for a learning rate warm-ups (linear warmup)
+            total_steps: total steps
+            cycle_factor: factor for adjusting a period T_i
+        """
         self.warmup_steps = warmup_steps
         self.total_steps = total_steps
-        self.cycles = cycles
+        self.cycle_factor = cycle_factor # 주기에 적용하는 factor인 듯함.
         super(WarmupCosineDecay, self).__init__(optimizer, self.lr_lambda, last_epoch, verbose)
     
     def lr_lambda(self, step):
+        # warm-up 적용
         if step < self.warmup_steps:
             return float(step) / float(max(1, self.warmup_steps))
         
-        #progress = min(1, max(0,(step - self.warmup_steps) / float(self.total_steps - self.warmup_steps))) # clipping
-        #return 0.5 * (1. + torch.cos(math.pi*progress)) # 이게 tensorflow 수식. 그래서 이거 어쩌란 거냐? vit-pytorch랑은 다른데..
+        # min_eta = 0, max_eta = initial_lr로 가정. (그렇게 되니까 lr_lambda를 그냥 쓰는 거겠지 -> lr_lambda 자체가 lambda function으로 초기 학습률값을 조작하는 방식으로 동작)
+        # 얘는 annealing이 아니라 그냥 전체 step을 하나의 주기로 보는 것 같음. annealing을 하나 더 구현해보자!
+        progress = min(1, max(0,(step - self.warmup_steps) / float(self.total_steps - self.warmup_steps))) # clipping (progress를 0과 1 사이로 강제)
+        return 0.5 * (1. + math.cos(math.pi*progress*float(self.cycle_factor)))
+
+
+class WarmupCosineAnnealing(LambdaLR):
+    """
+    Cosine Annealing after Linear Warm-Up.
+    For Cosine Annealing, minimum learning rate implicitly set to zero and maximum learning rate implicitly set to the initial learning rate.
+    """
+    def __init__(self, optimizer, warmup_steps, period, cycle_factor=.1, last_epoch=-1, verbose=False):
+        self.warmup_steps = warmup_steps
+        self.t_cur = 0
+        self.period = period
+        self.cycle_factor = float(cycle_factor)
+        super(WarmupCosineAnnealing, self).__init__(optimizer, self.lr_lambda, last_epoch, verbose)
+
+    def lr_lambda(self, step):
+        # warm-up
+        if step < self.warmup_steps:
+            return float(step) / float(max(1, self.warmup_steps))
         
-        # progress after warmup (이 부분 나중에 수정할 것)
-        progress = float(step - self.warmup_steps) / float(max(1, self.total_steps - self.warmup_steps))
-        return max(0.0, 0.5 * (1. + math.cos(math.pi * float(self.cycles) * 2.0 * progress)))
+        # cosine annealing
+        else:
+            progress = min(1, max(0, self.t_cur / self.period)) # clipping to range [0,1]
+            
+            if self.t_cur and self.t_cur % self.period == 0: # 한 주기가 끝나면
+                self.period *= (1+self.cycle_factor)
+                self.t_cur = 0
+            else:
+                self.t_cur += 1
+
+            return 0.5 * (1+math.cos(progress*math.pi))
